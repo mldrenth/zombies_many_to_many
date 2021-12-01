deb0837c6bf36dfa02ec72f7184a8844443a4026
Marijke Drenth <drenth.marijke@googlemail.com>
2021-12-01 20:45:33 +0200

    Added sql files

---
+++ queries.sql
@@ -0,0 +1,6 @@
+-- queries.sql
+SELECT victims.name, zombies.name, bitings.infected_on FROM victims
+INNER JOIN bitings
+ON bitings.victim_id = victims.id
+INNER JOIN zombies
+ON zombies.id = bitings.zombie_id;
